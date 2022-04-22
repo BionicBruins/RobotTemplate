1bc2fd117072ba69d193e872ee72c2e33e5c2329
Sptele <79765399+Sptele@users.noreply.github.com>
2022-04-22 12:09:45 -0700

    Templatize

--- src/srcMain.h
+++ src/srcMain.h
@@ -1,34 +1,26 @@
 #ifndef SRC_MAIN
 #define SRC_MAIN
 
-namespace batmobile {
+namespace MotorRobot {
     class MotorSet;
     class Robot;
     class ControllerScreen;
 }
 
 namespace config {
-  constexpr int max_speed_v = -85;
+  constexpr int max_speed = 100;
+  constexpr double modifier = 2/3;
 }
 
 namespace ports {
-    namespace individual {
-        // Change these depending on ports
-        constexpr int left_ports_top = 20;
-        constexpr int left_ports_bottom = 17;
+    // Change these depending on ports
+    constexpr int left_ports_top = 20;
+    constexpr int left_ports_bottom = 17;
 
-        constexpr int right_ports_top = 19;
-        constexpr int right_ports_bottom = 18;
-
-        constexpr int messi = 16;
-    }
-
-    namespace ind_ports = ports::individual;
+    constexpr int right_ports_top = 19;
+    constexpr int right_ports_bottom = 18;  
 
     std::vector<int> left_ports = std::vector<int> {ind_ports::left_ports_top, ind_ports::left_ports_bottom};
     std::vector<int> right_ports = std::vector<int> {ind_ports::right_ports_top, ind_ports::right_ports_bottom};
 }
-
-namespace ind_ports = ports::ind_ports;
-
 #endif
