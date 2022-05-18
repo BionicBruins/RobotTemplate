addd0984ffe172a69832b0fedb5bbecac4cfd849
Gautam <gautamkhaj07@gmail.com>
2022-05-17 21:55:02 -0700

    root

--- include/main.hpp
+++ include/main.hpp
@@ -1,25 +1,100 @@
 #pragma once
 
-namespace MotorRobot {
-    class MotorSet;
-    class Robot;
-    class ControllerScreen;
+#include <vector>
+
+namespace MotorRobot
+{
+	enum class ControlType
+	{
+		Tank,
+		Arcade,
+	};
+
+	class MotorSet
+	{
+	private:
+		std::vector<int> left_motors;
+		std::vector<int> right_motors;
+
+	public:
+		MotorSet();
+
+		MotorSet(std::vector<int> &left_motors_ports, std::vector<int> &right_motors_ports);
+
+		void move_left_wheels(int32_t voltage);
+
+		void move_right_wheels(int32_t voltage);
+	};
+
+	class Robot
+	{
+	private:
+		MotorSet binded;
+		pros::Controller master{pros::Controller(CONTROLLER_MASTER)};
+
+	public:
+		ControlType control_type = ControlType::Tank;
+
+		Robot();
+
+		Robot(MotorSet &motor_set, pros::controller_id_e_t controller_binding);
+
+		Robot(std::vector<int> &left_motors_ports, std::vector<int> &right_motors_ports, pros::controller_id_e_t controller_binding);
+
+		void tank_control();
+
+		void arcade_control();
+
+		/**
+			 * Uses the control scheme as mapped by the control_type.
+			 * 1 - tank control
+			 * 0 - arcade control
+			 */
+		void selected_control();
+
+		void switch_control_type();
+
+		pros::Controller &get_controller();
+
+		MotorSet &get_motor_set();
+	};
+
+	class ControllerScreen
+	{
+	private:
+		pros::Controller master{pros::Controller(CONTROLLER_MASTER)};
+
+	public:
+		ControllerScreen();
+
+		ControllerScreen(pros::controller_id_e_t controller_binding);
+
+		ControllerScreen(pros::Controller &controller);
+
+		void draw(int row, int column, std::string &text);
+
+		void draw(std::string &text);
+
+		const std::string &control_type_to_string(ControlType &control_type);
+	};
 }
 
-namespace config {
-  constexpr int max_speed = 100;
-  constexpr double modifier = 2/3;
-  constexpr double scale = (200/127);
+namespace config
+{
+	constexpr int max_speed = 100;
+	constexpr double modifier = 2 / 3;
+	constexpr double scale = (200 / 127);
 }
 
-namespace ports {
-    // Change these depending on ports
-    constexpr int left_ports_top = 20;
-    constexpr int left_ports_bottom = 17;
+namespace ports
+{
+	// Change these depending on ports
+	constexpr int left_ports_top = 20;
+	constexpr int left_ports_bottom = 17;
 
-    constexpr int right_ports_top = 19;
-    constexpr int right_ports_bottom = 18;  
+	constexpr int right_ports_top = 19;
+	constexpr int right_ports_bottom = 18;
 
-    std::vector<int> left_ports = std::vector<int> {left_ports_top, left_ports_bottom};
-    std::vector<int> right_ports = std::vector<int> {right_ports_top, right_ports_bottom};
+	std::vector<int> left_ports = std::vector<int>{left_ports_top, left_ports_bottom};
+	std::vector<int> right_ports = std::vector<int>{right_ports_top, right_ports_bottom};
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,158 +1,155 @@
 #include "main.h"
-#include "srcMain.h"
+#include "main.hpp"
 #include <vector>
 
-void initialize() {}
+namespace MotorRobot
+{
+
+	MotorSet::MotorSet() {}
+
+	MotorSet::MotorSet(std::vector<int> &left_motors_ports,
+					   std::vector<int> &right_motors_ports)
+	{
+		left_motors = left_motors_ports;
+		right_motors = right_motors_ports;
+	}
+
+	void MotorSet::move_left_wheels(int32_t voltage)
+	{
+		for (int &port_num : left_motors)
+		{
+			// Uses C lib
+			pros::c::motor_move_velocity(port_num, -voltage * config::scale * config::modifier);
+		}
+	}
+
+	void MotorSet::move_right_wheels(int32_t voltage)
+	{
+		for (int &port_num : right_motors)
+		{
+			// Uses C lib
+			pros::c::motor_move_velocity(port_num, voltage * config::scale * config::modifier);
+		}
+	}
+
+	Robot::Robot() {}
+
+	Robot::Robot(MotorSet &motor_set, pros::controller_id_e_t controller_binding)
+	{
+		binded = motor_set;
+		master = pros::Controller(controller_binding);
+	}
+
+	Robot::Robot(std::vector<int> &left_motors_ports, std::vector<int> &right_motors_ports,
+				 pros::controller_id_e_t controller_binding)
+	{
+		binded = MotorSet(left_motors_ports, right_motors_ports);
+		master = pros::Controller(controller_binding);
+	}
+
+	void Robot::tank_control()
+	{
+		binded.move_left_wheels(master.get_analog(ANALOG_LEFT_Y));
+		binded.move_right_wheels(master.get_analog(ANALOG_RIGHT_Y));
+	}
+
+	void Robot::arcade_control()
+	{
+		int power = master.get_analog(ANALOG_LEFT_Y);
+		int turn = master.get_analog(ANALOG_RIGHT_X);
+		int left = power + turn;
+		int right = power - turn;
+		binded.move_left_wheels(left);
+		binded.move_right_wheels(right);
+	}
+
+	void Robot::selected_control()
+	{
+		switch (control_type)
+		{
+		case ControlType::Arcade:
+			return arcade_control();
+		case ControlType::Tank:
+			return tank_control();
+		}
+	}
+
+	void Robot::switch_control_type()
+	{
+		switch (control_type)
+		{
+		case ControlType::Arcade:
+			control_type = ControlType::Tank;
+			break;
+		case ControlType::Tank:
+			control_type = ControlType::Arcade;
+			break;
+		}
+	}
+
+	pros::Controller &Robot::get_controller() { return master; }
+
+	MotorSet &Robot::get_motor_set() { return binded; }
+
+	ControllerScreen::ControllerScreen() {}
+
+	ControllerScreen::ControllerScreen(pros::controller_id_e_t controller_binding)
+		: master{controller_binding} {}
+
+	ControllerScreen::ControllerScreen(pros::Controller &controller) : master(controller) {}
+
+	void ControllerScreen::draw(int row, int column, std::string &text)
+	{
+		master.set_text(row, column, text);
+		pros::delay(50);
+	}
+
+	void ControllerScreen::draw(std::string &text)
+	{
+		master.set_text(0, 0, text);
+		pros::delay(50);
+	}
+
+	const std::string &ControllerScreen::control_type_to_string(ControlType &control_type) {
+		switch (control_type) {
+			case ControlType::Tank: return "Tank";
+			case ControlType::Arcade: return "Arcade";
+		}
+	}
+}; // namespace motorrobot
+
+
+MotorRobot::MotorSet motors(ports::left_ports, ports::right_ports);
+MotorRobot::Robot robot(motors, CONTROLLER_MASTER);
+MotorRobot::ControllerScreen lcd(robot.get_controller());
+
+void display() { 
+	lcd.draw("DM (Y): " + lcd.control_type_to_string(robot.control_type));
+}
+
+void initialize() {
+	pros::Task display_task (display);
+}
 
 void disabled() {}
 
 void competition_initialize() {}
 
-void autonomous() {
+void autonomous()
+{
 	// Put auto code here
 }
 
-namespace MotorRobot {
 
-class MotorSet {
-private:
-  std::vector<int> left_motors;
-  std::vector<int> right_motors;
+void opcontrol()
+{
+	while (true)
+	{
+		robot.selected_control();
 
-public:
-  MotorSet() {}
+		if (robot.get_controller().get_digital(DIGITAL_Y))
+			robot.switch_control_type();
 
-  MotorSet(std::vector<int> left_motors_ports,
-           std::vector<int> right_motors_ports) {
-    	left_motors = left_motors_ports;
-    	right_motors = right_motors_ports;
-  }
-
-  void move_left_wheels(int32_t voltage) {
-    for (int &port_num : left_motors) {
-		// Uses C lib
-		pros::c::motor_move_velocity(port_num, -voltage * config::scale * config::modifier);
+		pros::delay(2);
 	}
-  }
-
-  void move_right_wheels(int32_t voltage) {
-    for (int &port_num : right_motors) {
-      pros::c::motor_move_velocity(port_num, voltage * config::scale * config::modifier);
-    }
-  }
-};
-
-enum class ControlType {
-  Tank,
-  Arcade,
-};
-
-class Robot {
-private:
-  MotorSet binded;
-  pros::Controller master{ pros::Controller(CONTROLLER_MASTER) };
-
-public:
-  ControlType control_type = ControlType::Tank;
-
-public:
-  Robot(MotorSet motor_set, pros::controller_id_e_t controller_binding) {
-    binded = motor_set;
-    master = pros::Controller(controller_binding);
-  }
-
-  Robot(std::vector<int> left_motors_ports, std::vector<int> right_motors_ports,
-        pros::controller_id_e_t controller_binding) {
-    binded = MotorSet(left_motors_ports, right_motors_ports);
-    master = pros::Controller(controller_binding);
-  }
-
-  void tank_control() {
-    binded.move_left_wheels(master.get_analog(ANALOG_LEFT_Y));
-    binded.move_right_wheels(master.get_analog(ANALOG_RIGHT_Y));
-  }
-
-  void arcade_control() {
-    int power = master.get_analog(ANALOG_LEFT_Y);
-    int turn = master.get_analog(ANALOG_RIGHT_X);
-    int left = power + turn;
-    int right = power - turn;
-    binded.move_left_wheels(left);
-    binded.move_right_wheels(right);
-  }
-
-  /**
-   * Uses the control scheme as mapped by the control_type.
-   * 1 - tank control
-   * 0 - arcade control
-   */
-  void selected_control() {
-    switch (control_type) {
-    case ControlType::Arcade:
-      return arcade_control();
-    case ControlType::Tank:
-      return tank_control();
-    }
-  }
-
-  void switch_control_type() {
-    switch (control_type) {
-    case ControlType::Arcade:
-      control_type = ControlType::Tank;
-      break;
-    case ControlType::Tank:
-      control_type = ControlType::Arcade;
-      break;
-    }
-  }
-
-  pros::Controller &get_controller() { return master; }
-
-  MotorSet &get_motor_set() { return binded; }
-};
-
-class ControllerScreen {
-private:
-  pros::Controller master{ pros::Controller(CONTROLLER_MASTER) };
-  int count = 0;
-
-public:
-  ControllerScreen() {}
-
-  ControllerScreen(pros::controller_id_e_t controller_binding)
-      : master{controller_binding} {}
-
-  ControllerScreen(pros::Controller controller) : master(controller) {}
-
-  void draw(int row, int column, std::string text) {
-    if (!(count % 25)) {
-      master.set_text(row, column, text);
-    }
-    count++;
-  }
-
-  void draw(std::string text) {
-	static int myCount;
-	if (!(myCount % 25)) {
-    		master.set_text(0, 0, text);
-	}
-	myCount ++;
-  }
-};
-}; // namespace motorrobot
-
-void opcontrol() {
-  MotorRobot::MotorSet motors(ports::left_ports, ports::right_ports);
-  MotorRobot::Robot robot(motors, CONTROLLER_MASTER);
-  MotorRobot::ControllerScreen lcd(batmobile.get_controller());
-
-  while (true) {
-    	robot.selected_control();
-
-	if (batmobile.get_controller().get_digital(DIGITAL_Y)) {
-  	      batmobile.switch_control_type();
-	}
-	  
-    	pros::delay(2);
-  }
 }
