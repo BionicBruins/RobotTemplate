5b05397c241e918dd1c821d45204a107cba164f2
Sptele <79765399+Sptele@users.noreply.github.com>
2022-05-17 20:54:17 -0700

    Create main.hpp

---
+++ include/main.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace MotorRobot {
+    class MotorSet;
+    class Robot;
+    class ControllerScreen;
+}
+
+namespace config {
+  constexpr int max_speed = 100;
+  constexpr double modifier = 2/3;
+  constexpr double scale = (200/127);
+}
+
+namespace ports {
+    // Change these depending on ports
+    constexpr int left_ports_top = 20;
+    constexpr int left_ports_bottom = 17;
+
+    constexpr int right_ports_top = 19;
+    constexpr int right_ports_bottom = 18;  
+
+    std::vector<int> left_ports = std::vector<int> {left_ports_top, left_ports_bottom};
+    std::vector<int> right_ports = std::vector<int> {right_ports_top, right_ports_bottom};
+}
